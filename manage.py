b0dc616164c7c4420caf40795e458d42680a80c7
Hilli <Hilli@Hillis-MacBook-Air.local>
2016-04-26 19:27:12 +0100

    i just made a change

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os
 import sys
-# Husseins project
+# Husseins project with Mo
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "byoot.settings")
 
